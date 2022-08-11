4c831d573f1eec8cdcaaded019b4f0d2cdd1db34
AkiraSampei <s1290238@u-aizu.ac.jp>
2022-08-11 14:07:56 +0900

    re take2

--- sampleData.js
+++ sampleData.js
@@ -1,151 +1,150 @@
 //名前が変えても、中身が一緒ならOK！
-const postTemplate = {
-    eventList: [
+//テストデータ１　（下に2つ目がある）
+const postTemplate =
+{
+    eventID: "testID",
+    eventName: "発表会をブッチしよう",
+    dateList: [
         {
-            eventID: "testID",
-            eventName: "発表会をブッチしよう",
-            dateList: [
+            date: "8月11日",
+            userList: [
                 {
-                    date: "8月11日",
-                    userList: [
-                        {
-                            name: "matumoto",
-                            status: "NG"
-                        },
-                        {
-                            name: "tai",
-                            status: "OK"
-                        },
-                        {
-                            name: "あなた",
-                            status: "NG"
-                        }
-                    ]
+                    name: "matumoto",
+                    status: "NG"
                 },
                 {
-                    date: "8月12日",
-                    userList: [
-                        {
-                            name: "matumoto",
-                            status: "WARN"
-                        },
-                        {
-                            name: "tai",
-                            status: "OK"
-                        },
-                        {
-                            name: "あなた",
-                            status: "OK"
-                        }
-                    ]
+                    name: "tai",
+                    status: "OK"
                 },
                 {
-                    date: "8月13日",
-                    userList: [
-                        {
-                            name: "matumoto",
-                            status: "NG"
-                        },
-                        {
-                            name: "tai",
-                            status: "WARN"
-                        },
-                        {
-                            name: "あなた",
-                            status: "NG"
-                        }
-                    ]
-                },
-                {
-                    date: "8月14日",
-                    userList: [
-                        {
-                            name: "matumoto",
-                            status: "NG"
-                        },
-                        {
-                            name: "tai",
-                            status: "OK"
-                        },
-                        {
-                            name: "あなた",
-                            status: "NG"
-                        }
-                    ]
+                    name: "あなた",
+                    status: "NG"
                 }
             ]
         },
         {
-            eventID: "testID2",
-            eventName: "夏休みを謳歌しよう（したい）",
-            dateList: [
+            date: "8月12日",
+            userList: [
                 {
-                    date: "8月20日",
-                    userList: [
-                        {
-                            name: "matumoto",
-                            status: "WARN"
-                        },
-                        {
-                            name: "tai",
-                            status: "OK"
-                        },
-                        {
-                            name: "あなた",
-                            status: "NG"
-                        }
-                    ]
+                    name: "matumoto",
+                    status: "WARN"
                 },
                 {
-                    date: "8月30日",
-                    userList: [
-                        {
-                            name: "matumoto",
-                            status: "NG"
-                        },
-                        {
-                            name: "tai",
-                            status: "OK"
-                        },
-                        {
-                            name: "あなた",
-                            status: "OK"
-                        }
-                    ]
+                    name: "tai",
+                    status: "OK"
                 },
                 {
-                    date: "9月10日",
-                    userList: [
-                        {
-                            name: "matumoto",
-                            status: "NG"
-                        },
-                        {
-                            name: "tai",
-                            status: "OK"
-                        },
-                        {
-                            name: "あなた",
-                            status: "WARN"
-                        }
-                    ]
+                    name: "あなた",
+                    status: "OK"
+                }
+            ]
+        },
+        {
+            date: "8月13日",
+            userList: [
+                {
+                    name: "matumoto",
+                    status: "NG"
                 },
                 {
-                    date: "9月30日",
-                    userList: [
-                        {
-                            name: "matumoto",
-                            status: "NG"
-                        },
-                        {
-                            name: "tai",
-                            status: "OK"
-                        },
-                        {
-                            name: "あなた",
-                            status: "NG"
-                        }
-                    ]
+                    name: "tai",
+                    status: "WARN"
+                },
+                {
+                    name: "あなた",
+                    status: "NG"
+                }
+            ]
+        },
+        {
+            date: "8月14日",
+            userList: [
+                {
+                    name: "matumoto",
+                    status: "NG"
+                },
+                {
+                    name: "tai",
+                    status: "OK"
+                },
+                {
+                    name: "あなた",
+                    status: "NG"
+                }
+            ]
+        }
+    ]
+};
+//テストデータ２
+const postTemplate2 = {
+    eventID: "testID2",
+    eventName: "夏休みを謳歌しよう（したい）",
+    dateList: [
+        {
+            date: "8月20日",
+            userList: [
+                {
+                    name: "matumoto",
+                    status: "WARN"
+                },
+                {
+                    name: "tai",
+                    status: "OK"
+                },
+                {
+                    name: "あなた",
+                    status: "NG"
+                }
+            ]
+        },
+        {
+            date: "8月30日",
+            userList: [
+                {
+                    name: "matumoto",
+                    status: "NG"
+                },
+                {
+                    name: "tai",
+                    status: "OK"
+                },
+                {
+                    name: "あなた",
+                    status: "OK"
+                }
+            ]
+        },
+        {
+            date: "9月10日",
+            userList: [
+                {
+                    name: "matumoto",
+                    status: "NG"
+                },
+                {
+                    name: "tai",
+                    status: "OK"
+                },
+                {
+                    name: "あなた",
+                    status: "WARN"
+                }
+            ]
+        },
+        {
+            date: "9月30日",
+            userList: [
+                {
+                    name: "matumoto",
+                    status: "NG"
+                },
+                {
+                    name: "tai",
+                    status: "OK"
+                },
+                {
+                    name: "あなた",
+                    status: "NG"
                 }
             ]
         }
